ea8ae8f1ff27e52f73670bbd8f2d284b3c992095
MeherDeepak-2005 <68017128+MeherDeepak-2005@users.noreply.github.com>
2021-06-21 16:32:45 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,18 +4,6 @@ import girl from './img/landing-page-girl.png';
 import Search from './search'
 import logo from './img/logo.png'
 import youtubeApi from './api/api'
-import { assertNullLiteralTypeAnnotation } from '@babel/types';
-
-
-const express = require('express')
-const path = require('path');
-
-const app = express()
-const port = process.env.PORT || 3000 // Heroku will need the PORT environment variable
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.listen(port, () => console.log(`App is live on port ${port}!`))
 
 class App extends React.Component {
   state = {
